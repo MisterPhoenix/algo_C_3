5e8d1a31c121c5ea2ac907b5da0a03b0fe4ca091
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-13 14:04:54 +0000

    opti function

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int search_tab(){
+int display_depart(int tab[], int len){
+    for(int i = 0; i < 8; i++){
+        printf("[%d] ", tab[i]);
+    }
+}
+
+
+int main(){
     int idf[] = {75, 77, 78, 91, 92, 93, 94, 95};
 
     printf("la liste des départements de la région Île-de-France : ");
 
-    for(int i = 0; i < 8; i++)
-        printf("[%d] ", idf[i]);
+    display_depart(idf, 8);
 
     printf("\n");
-}
 
-int main(){
-    search_tab();
+    return 0;
 }
