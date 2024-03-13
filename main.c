1a4fc21ecd9a8db36e1b46dd93ce54fd52dae964
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-13 13:47:36 +0000

    fonction terminé

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int search_tab(int len){
+int search_tab(){
     int idf[] = {75, 77, 78, 91, 92, 93, 94, 95};
 
-    for(int i = 0; i < len; i++)
-        printf("%d\n", idf[i]);
+    printf("la liste des départements de la région Île-de-France : ");
+
+    for(int i = 0; i < 8; i++)
+        printf("[%d] ", idf[i]);
+
+    printf("\n");
 }
 
 int main(){
-    printf("%d", search_tab(8));
+    search_tab();
 }
