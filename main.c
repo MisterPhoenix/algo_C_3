03c0629628f950b6fdcce16eb20e684d5593854c
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-13 11:00:14 +0000

    recherche tableau reg

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
+#include "functions.h"
 
-int main(){}
+int main(){
+    int idf[] = {75, 77, 78, 91, 92, 93, 94, 95};
+
+    printf("La liste des département d'Île-de-France est ");
+    rech_tab(idf, 8);
+}
