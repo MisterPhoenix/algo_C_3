adb4871f9160ce58b6cd4ad6c3bdee2917b5dac3
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-13 13:37:15 +0000

    gbbgg

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ int search_tab(int len){
     int idf[] = {75, 77, 78, 91, 92, 93, 94, 95};
 
     for(int i = 0; i < len; i++)
-        printf("%d", idf[i]);
+        printf("%d\n", idf[i]);
 }
 
 int main(){
-    printf("la liste des département de la région ile de france : %d", search_tab(8));
+    printf("%d", search_tab(8));
 }
