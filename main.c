948a8956cc8c22a57fafe135078a85b454419a03
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-13 13:21:36 +0000

    recréa fun search idf

--- main.c
+++ main.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
 #include "functions.h"
+
+int main(){
+    int idf[] = {75, 77, 78, 91, 92, 93, 94, 95};
+
+    printf("la liste des département de la région ile de france : ");
+    search_tab(idf, 8);
+}
