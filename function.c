2149adc1f67daf7a80d1198fc5a192c7536ad14e
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-20 09:09:06 +0000

    créa strucutre fonction

--- function.c
+++ function.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include "function.h"
+
+int lenght(int t[]){
+    int len = 0;
+
+    for(int i = 0; t[i] != '\0'; i++){
+        len++;
+    }
+
+    return len;
+}
+
+void affich_depart(int t[], int len){
+    for(int i = 0; i < len; i = i+1){
+        printf("[%i]", t[i]);
+    }
+}
