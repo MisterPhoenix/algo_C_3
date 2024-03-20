511faa62b587f71a8d2bcbcdf3bd0812132db489
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-03-20 09:13:51 +0000

    cré main

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int main(){
+    int idf[] = {75, 77, 78, 91, 92, 93, 94, 95};
+
+    int aura[] = {1, 3, 7, 15, 26, 38, 42, 43, 63, 69, 73, 74};
+
+    int tab[1000];
+
+    printf("la liste des département d'Île-de-France :");
+
+    affich_depart(idf, 8);
+
+    printf("\n");
+}
