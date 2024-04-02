356f7df4358dba6ffd93e30f15999dcd53e333fe
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-04-02 14:24:37 +0000

    modif fun add dep

--- function.c
+++ function.c
@@ -14,7 +14,7 @@ int lenght(int t[]){
 
 //affichage des départements dans le tableau
 void affich_depart(int t[], int len){
-    for(int i = 0; i < len; i = i+1){
+    for(int i = 0; i < len, t[i] != 0; i++){
         printf("[%i]", t[i]);
     }
 }
@@ -47,4 +47,20 @@ int maxt(int t[]){
 }
 
 //fonction ajout d'un département dans un tableau
-int add_depart(int t[], int len, int y){}
+int add_depart(int t[], int len, int y){
+    if (search_depart(t, len, y)){
+        printf("Le département a été trouvé : ", y);
+        return 0;
+    }
+
+    for (int i = 0; i < TMAX; i++){
+        printf("Position de t[i] : %d %d\n", i, t[i]);
+        if(t[i] == 0){
+            t[i] = y;
+
+            printf("Département ajouté : %d\n", t[i]);
+            return 1;
+        }
+    }
+    
+}
