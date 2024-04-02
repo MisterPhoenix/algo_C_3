e2708cd1001cb4a8a5e41873b30223fbf688eb2f
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2024-04-02 14:42:38 +0000

    g

--- main.c
+++ main.c
@@ -45,7 +45,7 @@ int main(){
 
     affich_depart(disp, TMAX);
 
-    //add_depart(idf, TMAX, 78);
+    add_depart(idf, TMAX, 78);
 
-    //affich_depart(idf, TMAX);
+    affich_depart(idf, TMAX);
 }
